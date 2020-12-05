ca0c50aba8d57d2af02e8128e0eb6ed64b1cca00
ostwilkens <carl@ostwilkens.se>
2020-12-05 15:08:58 +0100

    day 5 part 1

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -1,3 +1,36 @@
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+};
+
 fn main() {
-    println!("hey world");
+    let file = File::open("day5/input.txt").unwrap();
+
+    let lines = BufReader::new(file)
+        .lines()
+        .filter_map(|l| l.ok())
+        .collect::<Vec<String>>();
+
+    let mut max_id = 0;
+    for line in lines {
+        let row = line[0..7]
+            .replace("F", "\x00")
+            .replace("B", "\x01")
+            .bytes()
+            .rev()
+            .enumerate()
+            .fold(0, |x, (i, b)| x | (b << i));
+
+        let col = line[7..10]
+            .replace("L", "\x00")
+            .replace("R", "\x01")
+            .bytes()
+            .rev()
+            .enumerate()
+            .fold(0, |x, (i, b)| x | (b << i));
+
+        max_id = max_id.max(row as u16 * 8 + col as u16);
+    }
+
+    println!("{:?}", max_id);
 }
