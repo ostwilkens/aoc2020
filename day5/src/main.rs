9cc707933b18ea1accf973a15edc74e921205c89
ostwilkens <carl@ostwilkens.se>
2020-12-05 15:37:17 +0100

    day 5 part 2

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -6,11 +6,14 @@ use std::{
 fn main() {
     let file = File::open("day5/input.txt").unwrap();
 
-    let lines = BufReader::new(file)
+    let mut lines = BufReader::new(file)
         .lines()
         .filter_map(|l| l.ok())
         .collect::<Vec<String>>();
 
+    lines.sort();
+
+    let mut prev_id = 0;
     let mut max_id = 0;
     for line in lines {
         let row = line[0..7]
@@ -29,8 +32,19 @@ fn main() {
             .enumerate()
             .fold(0, |x, (i, b)| x | (b << i));
 
-        max_id = max_id.max(row as u16 * 8 + col as u16);
+        let id = row as u16 * 8 + col as u16;
+
+        if id + 14 == prev_id {
+            println!("part 2: {:?}", prev_id - 15);
+        }
+
+        if id == prev_id + 2 {
+            println!("part 2: {:?}", prev_id + 1);
+        }
+
+        prev_id = id;
+        max_id = max_id.max(id);
     }
 
-    println!("{:?}", max_id);
+    println!("part 1: {:?}", max_id);
 }
